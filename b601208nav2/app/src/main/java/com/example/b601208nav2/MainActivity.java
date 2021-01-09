80a8e20f7af10efe7c60291cd23df8958d0a3b9b
mlapinm <mlapin@rambler.ru>
2021-01-09 19:10:00 +0300

    08 depricated Navigation Drawer with Fragments Part 2 - LAYOUT AND HAMBURGER ICON

---
+++ b601208nav2/app/src/main/java/com/example/b601208nav2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.b601208nav2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        
+    }
+
+}
