f4710cd210139072815b54f3c0e79536316a48be
mlapinm <mlapin@rambler.ru>
2021-01-09 11:20:25 +0300

    05 Custom Dialog + Sending Information to Activity - Android Studio Tutorial

---
+++ b601205frag/app/src/main/java/com/example/b601205frag/LittleDialog.java
@@ -0,0 +1,65 @@
+package com.example.b601205frag;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LittleDialog extends AppCompatDialogFragment  {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private LittleDialogListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view)
+                .setTitle("Login")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String username = editTextUsername.getText().toString();
+                        String password = editTextPassword.getText().toString();
+                        listener.applyTexts(username, password);
+                    }
+                });
+        editTextUsername = view.findViewById(R.id.edit_username);
+        editTextPassword = view.findViewById(R.id.edit_password);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (LittleDialogListener) context;
+        } catch (ClassCastException e) {
+            throw  new ClassCastException(context.toString() +
+                    "must implement LittleDialogListener");
+        }
+    }
+
+    public interface LittleDialogListener{
+        void applyTexts(String username, String password);
+    }
+}
