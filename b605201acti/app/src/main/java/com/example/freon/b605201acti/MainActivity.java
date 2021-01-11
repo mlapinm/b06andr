b6d6379a75be333187fc3a77f434f85e87677e77
Mikhail Lapin <mlapin@rambler.ru>
2021-01-11 17:03:25 +0300

    b605201acti How to Make a Button Open a New Activity

---
+++ b605201acti/app/src/main/java/com/example/freon/b605201acti/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.freon.b605201acti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
