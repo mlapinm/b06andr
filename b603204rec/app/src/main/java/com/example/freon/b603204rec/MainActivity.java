f787fc1e91817c688f05749baa1cb3f262da2c25
Mikhail Lapin <mlapin@rambler.ru>
2021-01-11 16:16:59 +0300

    b603204rec RecyclerView + CardView - Part 3 - INSERT & REMOVE ITEMS

---
+++ b603204rec/app/src/main/java/com/example/freon/b603204rec/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.freon.b603204rec;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recycleView);
+
+        ArrayList<ItemLittle> itemLittles = new ArrayList<>();
+        itemLittles.add(new ItemLittle(R.drawable.ic_android2, "Line 11", "Line 12"));
+        itemLittles.add(new ItemLittle(R.drawable.ic_audio, "Line 21", "Line 22"));
+        itemLittles.add(new ItemLittle(R.drawable.ic_sun, "Line 31", "Line 32"));
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new AdapterLittle(itemLittles);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+}
