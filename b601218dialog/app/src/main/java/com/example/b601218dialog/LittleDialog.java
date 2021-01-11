e45c062f40bad0b2f13e5128990f67c33214fd31
mlapinm <mlapin@rambler.ru>
2021-01-11 17:31:42 +0300

    b601218dialog

---
+++ b601218dialog/app/src/main/java/com/example/b601218dialog/LittleDialog.java
@@ -0,0 +1,27 @@
+package com.example.b601218dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LittleDialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Title")
+                .setMessage("This is Dialog")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
--- b601218dialog/app/src/main/java/com/example/b601218dialog/MainActivity.java
+++ b601218dialog/app/src/main/java/com/example/b601218dialog/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.b601218dialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    private void openDialog() {
+        LittleDialog littleDialog = new LittleDialog();
+        littleDialog.show(getSupportFragmentManager(), "little dialog");
     }
 }
