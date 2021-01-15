1dcaaf4cf621e8fe91d20f546a41ac7031b9a318
Mikhail Lapin <mlapin@rambler.ru>
2021-01-15 16:29:14 +0300

    b641012auto Custom AutoCompleteTextView

---
+++ b641012auto/app/src/main/java/com/example/freon/b641012auto/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.freon.b641012auto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<CountryItem> countryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fillCountryList();
+
+        AutoCompleteTextView editText = findViewById(R.id.actv);
+        AutoCompleteCountryAdapter adapter = new AutoCompleteCountryAdapter(this, countryList);
+        editText.setAdapter(adapter);
+    }
+
+    private void fillCountryList(){
+        countryList = new ArrayList<>();
+        countryList.add(new CountryItem("Afghanistan", R.drawable.flag_afghanistan));
+        countryList.add(new CountryItem("Albania", R.drawable.flag_albania));
+        countryList.add(new CountryItem("Algeria", R.drawable.flag_algeria));
+        countryList.add(new CountryItem("Andorra", R.drawable.flag_andorra));
+        countryList.add(new CountryItem("Angola", R.drawable.flag_angola));
+    }
+}
---
+++ b641014auto/app/src/main/java/com/example/freon/b641014auto/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.freon.b641014auto;
+
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<CountryItem> countryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fillCountryList();
+
+        AutoCompleteTextView editText = findViewById(R.id.actv);
+        AutoCompleteCountryAdapter adapter = new AutoCompleteCountryAdapter(this, countryList);
+        editText.setAdapter(adapter);
+    }
+
+    private void fillCountryList() {
+        countryList = new ArrayList<>();
+        countryList.add(new CountryItem("Afghanistan", R.drawable.flag_afghanistan));
+        countryList.add(new CountryItem("Albania", R.drawable.flag_albania));
+        countryList.add(new CountryItem("Algeria", R.drawable.flag_algeria));
+        countryList.add(new CountryItem("Andorra", R.drawable.flag_andorra));
+        countryList.add(new CountryItem("Angola", R.drawable.flag_angola));
+    }
+}
