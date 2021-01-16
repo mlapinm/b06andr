56b6236256a045d3ed556a5306ebdc903eeca539
mlapinm <mlapin@rambler.ru>
2021-01-16 14:43:45 +0300

    b609042pica How to Load an Image from a Url with Picasso

---
+++ b609042pica/app/src/main/java/com/example/b609042pica/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.b609042pica;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        String url = "https://cdn.pixabay.com/photo/2017/11/06/18/39/apple-2924531__480.jpg";
+        url = "https://cdn.pixabay.com/photo/2017/11/06/18/39/apple-2924531_1280.jpg";
+        Picasso.with(this).load(url).into(imageView);
+    }
+}
