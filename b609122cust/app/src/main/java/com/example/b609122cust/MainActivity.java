f66da8bcf9de2f5a66d5037e5dae22d22d2370da
mlapinm <mlapin@rambler.ru>
2021-01-17 17:22:01 +0300

    b609122cust How to Create a Custom Button (With Images)

---
+++ b609122cust/app/src/main/java/com/example/b609122cust/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.b609122cust;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button customButton = findViewById(R.id.custom_button);
+        Switch switchEnableButton = findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnableButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    customButton.setEnabled(true);
+                }else{
+                    customButton.setEnabled(false);
+                }
+            }
+        });
+    }
+}
