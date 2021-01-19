6f05b630e644f1f61f47aaf6c3809e17d9c09825
Mikhail Lapin <mlapin@rambler.ru>
2021-01-19 11:02:00 +0300

    B611052appbar How to Add an Up Button to the AppBar

---
+++ B611052appbar/app/src/main/java/com/example/freon/b611052appbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.freon.b611052appbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondActivity();
+            }
+        });
+    }
+
+    private void openSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
