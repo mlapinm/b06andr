56812f9198f859afec108438efe1582fc300c823
mlapinm <mlapin@rambler.ru>
2021-01-20 17:56:16 +0300

    B611232soft How to Hide the Soft Keyboard Programmatically

---
+++ B611232soft/app/src/main/java/com/example/b611232soft/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.b611232soft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickHide(View view) {
+        closeKeyboard();
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+}
