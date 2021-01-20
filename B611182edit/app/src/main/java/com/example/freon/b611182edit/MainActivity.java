f9fa1b492c11d243040891b8ee7f2e5a9705f753
Mikhail Lapin <mlapin@rambler.ru>
2021-01-20 14:05:11 +0300

    B611182edit How to Change the Input Method Action

--- B611182edit/app/src/main/java/com/example/freon/b611182edit/MainActivity.java
+++ B611182edit/app/src/main/java/com/example/freon/b611182edit/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.freon.b611182edit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText1 = findViewById(R.id.editText1);
+        EditText editText2 = findViewById(R.id.editText2);
+        editText1.setOnEditorActionListener(editorListener);
+        editText2.setOnEditorActionListener(editorListener);
     }
+
+    TextView.OnEditorActionListener editorListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            switch (i){
+                case EditorInfo.IME_ACTION_NEXT:
+                    Toast.makeText(MainActivity.this, "Next", Toast.LENGTH_SHORT).show();
+                    break;
+                case EditorInfo.IME_ACTION_SEND:
+                    Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return false;
+        }
+    };
 }
