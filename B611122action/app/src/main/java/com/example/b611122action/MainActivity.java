0eec611889f64d84034561d230b730ec86943812
mlapinm <mlapin@rambler.ru>
2021-01-20 08:22:53 +0300

    B611122action Contextual Action Mode

---
+++ B611122action/app/src/main/java/com/example/b611122action/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.b611122action;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionMode actionMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if(actionMode != null){
+                    return false;
+                }
+                actionMode = startSupportActionMode(callback);
+                return true;
+            }
+        });
+    }
+
+    private ActionMode.Callback callback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.little_menu, menu);
+            mode.setTitle("Choose your option");
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.option_1:
+                    Toast.makeText(MainActivity.this,
+                            "Option 1 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.option_2:
+                    Toast.makeText(MainActivity.this,
+                            "Option 2 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null;
+        }
+    };
+}
