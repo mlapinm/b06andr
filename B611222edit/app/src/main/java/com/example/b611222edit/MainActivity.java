7d671ab8fb0b71c5eb2ee76586c15e4ab573f8a8
mlapinm <mlapin@rambler.ru>
2021-01-20 17:28:39 +0300

    B611222edit Validate Email & Password with Regular Expression

---
+++ B611222edit/app/src/main/java/com/example/b611222edit/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.b611222edit;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    "(?=.*[a-z])" +         //at least 1 lower case letter
+                    "(?=.*[A-Z])" +         //at least 1 upper case letter
+//                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{4,}" +               //at least 4 characters
+                    "$");
+
+
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputLogin;
+    private TextInputLayout textInputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textInputEmail = findViewById(R.id.text_input_email);
+        textInputLogin = findViewById(R.id.text_input_login);
+        textInputPassword = findViewById(R.id.text_input_password);
+
+
+    }
+
+    private boolean validEmail() {
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validLogin() {
+        String loginInput = textInputLogin.getEditText().getText().toString().trim();
+        if (loginInput.isEmpty()) {
+            textInputLogin.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputLogin.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validPassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            textInputPassword.setError("Password too weak");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void onClickConfirm(View view) {
+        if (!validEmail() | !validLogin() | !validPassword()) {
+            return;
+        }
+        String input = "Email: " + textInputEmail.getEditText().getText().toString();
+        input += "\n";
+        input += "Username: " + textInputLogin.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    }
+}
