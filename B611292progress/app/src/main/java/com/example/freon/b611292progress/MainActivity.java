7b6664d75c12d39e8e7c372d73d07d7c5c026a64
mlapinm <mlapin@rambler.ru>
2021-01-21 20:57:02 +0300

    B611292progress Circular Determinate ProgressBar

--- B611292progress/app/src/main/java/com/example/freon/b611292progress/MainActivity.java
+++ B611292progress/app/src/main/java/com/example/freon/b611292progress/MainActivity.java
@@ -4,28 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    final static int DELTA = 10;
+    ProgressBar progressBar;
+
+    int progress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        progressBar = findViewById(R.id.progessBar);
+        progress = progressBar.getProgress();
+
     }
 
-    public void onClickRemove(View view) {
-        String text = textView.getText().toString();
-        text = text.substring(0, text.length()-1);
-        textView.setText(text);
+    public void onClickMines(View view) {
+        if(progress == 0) {
+            progress = 0;
+        } else {
+            progress -= DELTA;
+        }
+        progressBar.setProgress(progress);
     }
 
-    public void onClickAppend(View view) {
-        String text = textView.getText().toString();
-        text = text + "a";
-        textView.setText(text);
+
+    public void onClickPlus(View view) {
+        if(progress == 100) {
+            progress = 100;
+        } else {
+            progress += DELTA;
+        }
+        progressBar.setProgress(progress);
     }
 }
