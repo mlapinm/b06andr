6c233ae7881365ba3eb1b609e0ffcc357a80c124
Mikhail Lapin <mlapin@rambler.ru>
2021-01-21 16:46:33 +0300

    B611312text

---
+++ B611292progress/app/src/main/java/com/example/freon/b611292progress/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.freon.b611292progress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickRemove(View view) {
+
+    }
+
+    public void onClickAppend(View view) {
+
+    }
+
+}
