8677b5451f6f02c5936c80f8bae4569ec0666c95
Mikhail Lapin <mlapin@rambler.ru>
2021-01-21 16:52:38 +0300

    B611312text

--- B611292progress/app/src/main/java/com/example/freon/b611292progress/MainActivity.java
+++ B611292progress/app/src/main/java/com/example/freon/b611292progress/MainActivity.java
@@ -4,21 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
     }
 
     public void onClickRemove(View view) {
-
+        String text = textView.getText().toString();
+        text = text.substring(0, text.length()-1);
+        textView.setText(text);
     }
 
     public void onClickAppend(View view) {
-
+        String text = textView.getText().toString();
+        text = text + "a";
+        textView.setText(text);
     }
-
 }
