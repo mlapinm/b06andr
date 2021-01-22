9e7440e11ec8af148658caf091923cfba129d158
mlapinm <mlapin@rambler.ru>
2021-01-22 17:47:42 +0300

    B611383text AutoCompleteTextView

--- B611383text/app/src/main/java/com/example/freon/b611383text/MainActivity.java
+++ B611383text/app/src/main/java/com/example/freon/b611383text/MainActivity.java
@@ -7,21 +7,18 @@ import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String[] COUNTRIES = new String[]{
-            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        String[] countries = getResources().getStringArray(R.array.countries);
+        String[] countries = getResources().getStringArray(R.array.countries);
         AutoCompleteTextView editText = findViewById(R.id.actv);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,  COUNTRIES);
+                R.layout.item_layout, R.id.textView_item,  countries);
         editText.setAdapter(adapter);
-        //editText.setThreshold(1);
+        editText.setThreshold(1);
         //get the input like for a normal EditText
         //String input = editText.getText().toString();
     }
