c2f3acdd5e560c1f4fe8484d38237fd1e558934c
Mikhail Lapin <mlapin@rambler.ru>
2021-01-22 16:10:00 +0300

    B611372video B611373video  VideoView

--- B611372video/app/src/main/java/com/example/freon/b611372video/MainActivity.java
+++ B611372video/app/src/main/java/com/example/freon/b611372video/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.freon.b611372video;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://"
+                + getPackageName()
+                + "/"
+                + R.raw.month;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
     }
 }
---
+++ B611373video/app/src/main/java/com/example/freon/b611373video/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.freon.b611373video;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://"
+                + getPackageName()
+                + "/"
+                + R.raw.troyka;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+}
