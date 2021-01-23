91bf952ed57585969794ead72c08c940cc962f55
mlapinm <mlapin@rambler.ru>
2021-01-23 15:19:33 +0300

    B611492splash Easy Splash Screen

---
+++ B611492splash/app/src/main/java/com/example/b611492splash/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.b611492splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ B611492splash/app/src/main/java/com/example/b611492splash/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.b611492splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#1a1b29"))
+                .withHeaderText("Header")
+                .withFooterText("Footer")
+                .withBeforeLogoText("Before Logo Text")
+                .withAfterLogoText("After Logo text")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+    }
+}
