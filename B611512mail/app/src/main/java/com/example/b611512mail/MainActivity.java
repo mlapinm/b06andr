5e8e462c01da8e3bb5de8edfcc4fb1f3772df5a1
mlapinm <mlapin@rambler.ru>
2021-01-23 17:25:58 +0300

    B611512mail How to Send an Email via Intent

---
+++ B611512mail/app/src/main/java/com/example/b611512mail/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.b611512mail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextMail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMail = findViewById(R.id.editTextMail);
+        editTextSubject = findViewById(R.id.editTextSubject);
+        editTextMessage = findViewById(R.id.editTextMessage);
+
+
+    }
+
+    public void sendEmail(View view) {
+        String recipientList = editTextMail.getText().toString();
+        String[] recipients = recipientList.split("\\s*,\\s*");
+
+        String subject = editTextSubject.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+    }
+}
