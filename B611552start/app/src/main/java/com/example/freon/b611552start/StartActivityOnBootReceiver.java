cf670517db7380e986a01d210d6a9f8bd90b07dc
Mikhail Lapin <mlapin@rambler.ru>
2021-01-25 10:38:42 +0300

    B611542screen How to Keep the Screen ON in an Android Activity

---
+++ B611552start/app/src/main/java/com/example/freon/b611552start/StartActivityOnBootReceiver.java
@@ -0,0 +1,16 @@
+package com.example.freon.b611552start;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartActivityOnBootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+        Intent intent1 = new Intent(context, MainActivity.class);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent1);
+        }
+    }
+}
