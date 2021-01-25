454f2ba855861baf38f7c7632ff5addbe03d46f0
Mikhail Lapin <mlapin@rambler.ru>
2021-01-25 10:59:53 +0300

    B611562repeat Delay and Repeat Code Execution with Handler PostDelayed

--- B611552start/app/src/main/java/com/example/freon/b611552start/StartActivityOnBootReceiver.java
+++ B611552start/app/src/main/java/com/example/freon/b611552start/StartActivityOnBootReceiver.java
@@ -7,10 +7,10 @@ import android.content.Intent;
 public class StartActivityOnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
         Intent intent1 = new Intent(context, MainActivity.class);
         intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent1);
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
         }
     }
 }
--- B611562repeat/app/src/main/java/com/example/freon/b611562repeat/MainActivity.java
+++ B611562repeat/app/src/main/java/com/example/freon/b611562repeat/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.freon.b611562repeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onStart(View view) {
+        runnable.run();
+    }
+
+    public void onStop(View view) {
+        handler.removeCallbacks(runnable);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(MainActivity.this,
+                    "This is delayed toast", Toast.LENGTH_SHORT).show();
+            handler.postDelayed(this, 4000);
+        }
+    };
 }
