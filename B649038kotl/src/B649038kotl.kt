dfe12a28ba9b5c59e6d6145bfbf7a4efebf99b37
Mikhail Lapin <mlapin@rambler.ru>
2021-01-28 12:25:51 +0300

    b6492kotl

--- B649038kotl/src/B649038kotl.kt
+++ B649038kotl/src/B649038kotl.kt
@@ -1,4 +1,4 @@
-fun p03hello(){
+fun p03hello() {
     println("Hello")
 }
 //Hello
@@ -36,43 +36,43 @@ Walther" """
 //3
 
 //Part 6 – Arithmetic Operators
-fun p06arithmetic(){
-        var a = 5
-        val b = 3.0
+fun p06arithmetic() {
+    var a = 5
+    val b = 3.0
 
-        println("a = " + a + " b = " + b)
+    println("a = " + a + " b = " + b)
 
-        println("a + b = ${a + b}")
-        println("a - b = ${a - b}")
-        println("a * b = ${a * b}")
-        println("a / b = ${a / b}")
-        println("a % b = ${a % b}")
+    println("a + b = ${a + b}")
+    println("a - b = ${a - b}")
+    println("a * b = ${a * b}")
+    println("a / b = ${a / b}")
+    println("a % b = ${a % b}")
 
-        println("(2 + 2) * 3 = ${(2 + 2) * 3}")
+    println("(2 + 2) * 3 = ${(2 + 2) * 3}")
 
-        println("-a = ${-a}")
+    println("-a = ${-a}")
 
-        a += 2
-        println("a += 2: $a")
+    a += 2
+    println("a += 2: $a")
 
-        println("a++: ${a++}")
-        println("++a: ${++a}")
+    println("a++: ${a++}")
+    println("++a: ${++a}")
 
-        a -= 2
-        println("a -= 2: $a")
+    a -= 2
+    println("a -= 2: $a")
 
-        println("a--: ${a--}")
-        println("--a: ${--a}")
+    println("a--: ${a--}")
+    println("--a: ${--a}")
 
-        a *= 2
-        println("a *= 2: $a")
+    a *= 2
+    println("a *= 2: $a")
 
-        a /= 2
-        println("a /= 2: $a")
+    a /= 2
+    println("a /= 2: $a")
 
-        a %= 2
-        println("a %= 2: $a")
-        println((8/6).toInt())
+    a %= 2
+    println("a %= 2: $a")
+    println((8 / 6).toInt())
 }
 //a--: 7
 //--a: 5
@@ -82,12 +82,12 @@ fun p06arithmetic(){
 //1
 
 //Part 7 – REPL
-fun p07repl(){
+fun p07repl() {
 
 }
 
 //Part 8 – Comments
-fun p08comment(){
+fun p08comment() {
 //
 /*
 
@@ -96,14 +96,14 @@ fun p08comment(){
 }
 
 //Part 9 – Arrays
-fun p09arrays(){
+fun p09arrays() {
     val names = arrayOf("John", "Jack", "Janny")
 
     println("number of elements: ${names.size}")
 
     println("first element: ${names[0]}")
 
-    println("last element: ${names[names.size-1]}")
+    println("last element: ${names[names.size - 1]}")
 
     println("1 & 2 : ${names.slice(1..2)}")
 
@@ -115,19 +115,19 @@ fun p09arrays(){
 //1 & 2 : [Jack, Janny]
 
 //Part 10 – For-Loop & Ranges
-fun p10loop(){
+fun p10loop() {
     var units = arrayOf("1", "2", "3")
     units.reverse()
     println(units[0])
-    for(i in units) print("${i} " + " - ")
+    for (i in units) print("${i} " + " - ")
     println()
-    for(i in 3..8) print( "" + i + " ")
+    for (i in 3..8) print("" + i + " ")
     println()
-    for(i in 11 until 15) print("" + i + " ")
+    for (i in 11 until 15) print("" + i + " ")
     println()
-    for(i in 6L downTo 1) print("" + i + " ")
+    for (i in 6L downTo 1) print("" + i + " ")
     println()
-    for(i in 'a'..'z') print(i)
+    for (i in 'a'..'z') print(i)
 }
 //3
 //3  - 2  - 1  -
@@ -138,7 +138,7 @@ fun p10loop(){
 
 
 //Part 11 – Comparison & Logical Operators
-fun p11logic(){
+fun p11logic() {
     println("2 > 1 is ${2 > 1}")
     println("2 < 1 is ${2 < 1}")
     println("1 >= 1 is ${1 >= 1}")
@@ -168,29 +168,29 @@ fun p11logic(){
 
 
 //Part 12 – While-Loop & Labels
-fun p12while(){
+fun p12while() {
     var i = 0
 
-    outer@ do{
+    outer@ do {
         print("" + i++)
-        for(j in 1..3){
+        for (j in 1..3) {
             println(j)
             break@outer
         }
-    }while (i<5)
+    } while (i < 5)
 
 }
 //1
 //0
 
 //Part 13 – If Else
-fun p13if(){
-    for (i in -2..10){
-        val prefix = if (i == 1){
+fun p13if() {
+    for (i in -2..10) {
+        val prefix = if (i == 1) {
             "*"
-        }else if (i !in 4..7){
+        } else if (i !in 4..7) {
             "+"
-        }else {
+        } else {
             print("-")
             ">"
         }
@@ -212,7 +212,7 @@ fun p13if(){
 //+ 10
 
 //Part 14 – When
-fun p14when(){
+fun p14when() {
     val time = 14
     val isSunday = true
 
@@ -226,7 +226,7 @@ fun p14when(){
 //I'm gonna stay in bed all day!
 
 //Part 15 – Function Parameters
-fun p15param(){
+fun p15param() {
     fun greet(name: String, reps: Int) {
         var repsLeft = reps
 
@@ -235,73 +235,121 @@ fun p15param(){
             repsLeft--
         }
     }
-        greet("Herbert", 2)
+    greet("Herbert", 2)
 }
 //Hello, Herbert! Nice to meet you!
 //Hello, Herbert! Nice to meet you!
 
 //Part 16 – Function Return Values
-fun p16return(){
+fun p16return() {
+
+    fun getMax(num1: Int, num2: Int): Int {
+        if (num1 > num2) {
+            return num1
+        } else {
+            return num2
+        }
+    }
+
+    val max = getMax(12, 2)
+    println(max)
 
 }
 
 //Part 17 – Single-Expression Functions
-fun p17alfa(){
+fun p17alfa() {
+
+    fun double(num: Int) = num * 2
+    println(double(12))
 
 }
 
 //Part 18 – Function Overloading
-fun p18func(){
+fun p18func() {
+    fun getMax(a: Int, b: Int) = if (a > b) a else b
 
+    fun getMax(a: Double, b: Double) = if (a > b) a else b
+
+    fun getMax(a: Int, b: Int, c: Int) =
+        when {
+            a >= b && a >= c -> a
+            b >= a && b >= c -> b
+            else -> c
+        }
+    println(getMax(14, 100, 9))
 }
 
 //Part 19 – Default & Named Arguments
-fun p19default(){
+fun p19default() {
+    fun greet(message: String, name: String = "User", reps: Int = 1) {
+        for (i in 0 until reps) println("Hello, $name! $message")
+    }
+
+    greet(
+        "How are you?"
+    )
 
 }
+//Hello, User! How are you?
 
 //Part 20 – Vararg & Spread Operator
-fun p20var(){
+fun p20var() {
+    fun getSum(a: Int, vararg numbers: Int, b: Int): Int {
+        var sum = a + b
+
+        for (number in numbers) sum += number
+
+        numbers[0] = 7
+        return sum
+    }
+
+    val array = intArrayOf(1, 2, 3, 4, 5, 6)
+
+    println(getSum(10, 20, *array, 30, b = 40))
+    println(array[0])
 
 }
+//121
+//1
 
 //Part 21 – Top-Level Variables & Const Val
-fun p21const(){
+const val STRING_CONSTANT = "I am a constant"
 
+fun p21const() {
+       println(STRING_CONSTANT)
 }
 
 //Part 22 – What Are Classes/OOP?
-fun p22classes(){
+fun p22classes() {
 
 }
 
 //Part 23 – Simple Class
-fun p23class(){
+fun p23class() {
 
 }
 
 //Part 24 – Init Blocks
-fun p24block(){
+fun p24block() {
 
 }
 
-fun p25(){
+fun p25() {
 
 }
 
-fun p26(){
+fun p26() {
 
 }
 
-fun p27(){
+fun p27() {
 
 }
-
 
 
 fun main() {
 
-    when(15) {
+    when (21) {
         3 -> p03hello()
         4 -> p04variables()
         5 -> p05types()
