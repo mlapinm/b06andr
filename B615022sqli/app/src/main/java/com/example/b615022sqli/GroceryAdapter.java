58999da101000022a13b6d8595bcfd99dce2fc06
mlapinm <mlapin@rambler.ru>
2021-01-30 13:18:02 +0300

    B615022sqli SQLite + RecyclerView - Part 2 - CURSOR AND RECYCLERVIEW ADAPTER

---
+++ B615022sqli/app/src/main/java/com/example/b615022sqli/GroceryAdapter.java
@@ -0,0 +1,71 @@
+package com.example.b615022sqli;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GroceryAdapter extends RecyclerView.Adapter<GroceryAdapter.GroceryViewHolder> {
+    private Context context;
+    private Cursor cursor;
+
+    public GroceryAdapter(Context context, Cursor cursor) {
+        this.context = context;
+        this.cursor = cursor;
+    }
+
+    public class GroceryViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName;
+        public TextView textViewCount;
+
+        public GroceryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.text_view_name_item);
+            textViewCount = itemView.findViewById(R.id.text_view_amount_item);
+        }
+    }
+
+    @NonNull
+    @Override
+    public GroceryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.grocery_item,
+                parent, false);
+        return new GroceryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GroceryViewHolder holder, int position) {
+        if(!cursor.moveToPosition(position)){
+            return;
+        }
+
+        String name = cursor.getString(cursor.getColumnIndex(GroceryContract.GroceryEntry.COLUMN_NAME));
+        int amount = cursor.getInt(cursor.getColumnIndex(GroceryContract.GroceryEntry.COLUMN_AMOUNT));
+
+        holder.textViewName.setText(name);
+        holder.textViewCount.setText(String.valueOf(amount));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    public void  swapCursor(Cursor newCursor){
+        if(cursor != null){
+            cursor.close();
+        }
+        cursor = newCursor;
+        if(newCursor != null){
+            notifyDataSetChanged();
+        }
+    }
+
+}
--- B615022sqli/app/src/main/java/com/example/b615022sqli/MainActivity.java
+++ B615022sqli/app/src/main/java/com/example/b615022sqli/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.b615022sqli;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase database;
+    private GroceryAdapter adapter;
+    private EditText editTextName;
+    private TextView textViewAmount;
+    private int amount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GroceryDBHelper dbHelper = new GroceryDBHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new GroceryAdapter(this, getAllItems());
+        recyclerView.setAdapter(adapter);
+
+        editTextName = findViewById(R.id.edit_text_name);
+        textViewAmount = findViewById(R.id.text_view_amount);
+
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+
+    }
+
+    public void onDecrease(View view) {
+        amount--;
+        textViewAmount.setText(String.valueOf(amount));
+    }
+
+    public void onIncrease(View view) {
+        amount++;
+        textViewAmount.setText(String.valueOf(amount));
+    }
+
+    public void onAdd(View view) {
+        if(editTextName.getText().toString().trim().length() == 0 || amount == 0){
+            return;
+        }
+        String name = editTextName.getText().toString().trim();
+        ContentValues cvs = new ContentValues();
+        cvs.put(GroceryContract.GroceryEntry.COLUMN_NAME, name);
+        cvs.put(GroceryContract.GroceryEntry.COLUMN_AMOUNT, amount);
+
+        database.insert(GroceryContract.GroceryEntry.TABLE_NAME, null, cvs);
+        adapter.swapCursor(getAllItems());
+    }
+
+    private Cursor getAllItems() {
+        return  database.query(
+                GroceryContract.GroceryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                GroceryContract.GroceryEntry.COLUMN_TIMESTAMP + " DESC"
+        );
     }
 }
