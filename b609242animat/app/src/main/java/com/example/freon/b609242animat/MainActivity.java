5b52fdf6403c958555c29110a92ae859c940a331
mlapinm <mlapin@rambler.ru>
2021-01-30 12:06:44 +0300

    b609252imag ConstraintLayout Tutorial Part 6 - CONSTRAINTSETS

--- b609242animat/app/src/main/java/com/example/freon/b609242animat/MainActivity.java
+++ b609242animat/app/src/main/java/com/example/freon/b609242animat/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.freon.b609242animat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Placeholder;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.transition.TransitionManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Placeholder placeholder;
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        placeholder = findViewById(R.id.placeholder);
+        layout = findViewById(R.id.layout);
+    }
+
+    public void swapView(View view) {
+
+        TransitionManager.beginDelayedTransition(layout);
+        placeholder.setContentId(view.getId());
+
     }
 }
