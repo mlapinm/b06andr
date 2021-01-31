a22c6996c659fd99fba7c50e7ae55d94dd2b40c7
mlapinm <mlapin@rambler.ru>
2021-01-31 18:41:40 +0300

    B653042mvvm

---
+++ B653042mvvm/app/src/main/java/com/example/b653042mvvm/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.example.b6532mvvm.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.example.b653042mvvm.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+}
