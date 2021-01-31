b8669d971bd75ebdf694f971c51da0f9a9982d0c
mlapinm <mlapin@rambler.ru>
2021-01-31 12:14:40 +0300

    B651012kotl Adapter & ViewHolder - Simple RecyclerView in Kotlin (2020 with AndroidX) Part 2

--- B651022kotl/app/src/main/java/com/example/b651022kotl/LittleAdapter.kt
+++ B651022kotl/app/src/main/java/com/example/b651022kotl/LittleAdapter.kt
@@ -1,11 +1,43 @@
 package com.example.b651022kotl
 
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LittleAdapter : RecyclerView.Adapter() {
+class LittleAdapter(
+        private val items: List<LittleItem>
+) : RecyclerView.Adapter<LittleAdapter.LittleViewHolder>() {
 
-    class LittleViewHolder: RecyclerView.ViewHolder{
+    class LittleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+        val textView1: TextView = itemView.findViewById(R.id.text_view_1)
+        val textView2: TextView = itemView.findViewById(R.id.text_view_2)
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LittleViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.little_item, parent, false)
+        return  LittleViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: LittleViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.imageView.setImageResource(item.imageResource)
+        holder.textView1.setText(item.text1)
+        holder.textView2.setText(item.text2)
+
+        if(position == 0){
+            holder.textView1.setBackgroundColor(Color.YELLOW)
+        }
+    }
+
+    override fun getItemCount() = items.size
+
 
 }
--- B651022kotl/app/src/main/java/com/example/b651022kotl/MainActivity.kt
+++ B651022kotl/app/src/main/java/com/example/b651022kotl/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.b651022kotl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerView: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = generateDummyList(50)
+
+        recyclerView = findViewById(R.id.recycle_view)
+        recyclerView?.adapter = LittleAdapter(items)
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+        recyclerView?.setHasFixedSize(true)
+
+
+
     }
+
+    private fun generateDummyList(size: Int): List<LittleItem>{
+        val items = ArrayList<LittleItem>()
+
+        for(i in 0 until size){
+            val image = when(i % 3){
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_audio
+                else -> R.drawable.ic_sun
+            }
+            val item = LittleItem(image, "Item $i", "Line 2")
+            items += item
+        }
+        return items
+    }
+
 }
