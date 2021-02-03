7ce00d6531ad8214d34ed8b9aa97aebe49c7a419
Mikhail Lapin <mlapin@rambler.ru>
2021-02-03 16:10:10 +0300

    com.example.freon.b6470725broad

--- B6470725broad/app/src/main/java/com/example/freon/b6470725broad/MainActivity.java
+++ B6470725broad/app/src/main/java/com/example/freon/b6470725broad/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.freon.b6470725broad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    OrderedReceiver2 orderedReceiver2 = new OrderedReceiver2();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter("com.example.freon.b6470725broad.LITTLE_ACTION");
+        filter.setPriority(1);
+        registerReceiver(orderedReceiver2, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(orderedReceiver2);
     }
 }
