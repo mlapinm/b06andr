9e44fe4b1dd95bc43576135b8766beec7f179e9a
Mikhail Lapin <mlapin@rambler.ru>
2021-02-04 15:29:02 +0300

    b609252imag ConstraintLayout Tutorial Part 6 - CONSTRAINTSETS

--- b609254imag/app/src/main/java/com/example/freon/b609254imag/MainActivity.java
+++ b609254imag/app/src/main/java/com/example/freon/b609254imag/MainActivity.java
@@ -1,8 +1,5 @@
-package com.example.b609252imag;
+package com.example.freon.b609254imag;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.transition.ChangeBounds;
@@ -10,13 +7,17 @@ import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
     private ConstraintLayout layout;
     private ConstraintSet constraintSetOld = new ConstraintSet();
     private ConstraintSet constraintSetNew = new ConstraintSet();
     private boolean altLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
         constraintSetOld.clone(layout);
         constraintSetNew.clone(this, R.layout.activity_main_alt);
-
     }
 
-    public void swapView(View view) {
-        Toast.makeText(this, "swapView", Toast.LENGTH_SHORT).show();
+    public void swapView(View v) {
         Transition changeBounds = new ChangeBounds();
         changeBounds.setInterpolator(new OvershootInterpolator());
 
         TransitionManager.beginDelayedTransition(layout, changeBounds);
 
-        if(!altLayout){
+        if (!altLayout) {
             constraintSetNew.applyTo(layout);
             altLayout = true;
-        }else{
+        } else {
             constraintSetOld.applyTo(layout);
             altLayout = false;
         }
-
     }
 }
