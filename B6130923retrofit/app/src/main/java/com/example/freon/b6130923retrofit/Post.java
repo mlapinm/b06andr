e9f92dd54da77359ec6c3e7cd74a69e38224b246
Mikhail Lapin <mlapin@rambler.ru>
2021-02-05 16:12:23 +0300

    B6130923retrofit Retrofit Tutorial Part 3 - POST REQUEST & FORM URLENCODED

---
+++ B6130923retrofit/app/src/main/java/com/example/freon/b6130923retrofit/Post.java
@@ -0,0 +1,34 @@
+package com.example.freon.b6130923retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
