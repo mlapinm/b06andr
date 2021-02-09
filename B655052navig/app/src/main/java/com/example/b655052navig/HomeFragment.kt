31729d0396ff9cb5250f72e4eed2be3da9fe4283
Mikhail Lapin <mlapin@rambler.ru>
2021-02-09 15:09:39 +0300

    B655052navig

---
+++ B655052navig/app/src/main/java/com/example/b655052navig/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.b655052navig
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var buttonLogin: Button? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonLogin = view.findViewById(R.id.buttonLogin)
+        buttonLogin?.setOnClickListener {
+
+            view.findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+
+//            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+//            findNavController().navigate(action)
+        }
+
+    }
+}
