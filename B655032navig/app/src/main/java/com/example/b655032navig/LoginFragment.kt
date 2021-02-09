ce5fd43fa42720070a54682a56dd8ff48a1704da
Mikhail Lapin <mlapin@rambler.ru>
2021-02-09 13:51:52 +0300

    B655032navig Navigating to Another Fragment - Getting Started With Navigation Component #3

---
+++ B655032navig/app/src/main/java/com/example/b655032navig/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.b655032navig
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
---
+++ B655042navig/app/src/main/java/com/example/b655042navig/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.b655042navig
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
