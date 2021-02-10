39f2633e0dec9307fe1974678335979156c396d7
mlapinm <mlapin@rambler.ru>
2021-02-10 20:39:40 +0300

    12

--- B655052navig/app/src/main/java/com/example/b655052navig/MainActivity.kt
+++ B655052navig/app/src/main/java/com/example/b655052navig/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.b655052navig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private var toolbar : Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        toolbar = findViewById(R.id.toolBar)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
